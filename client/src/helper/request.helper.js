4a1aa3b307e77465d706f2128c6dad9a4ed0c25d
nguyencongthang2509 <congthang25092003@gmail.com>
2023-08-04 20:48:29 +0700

    update api common

--- client/src/helper/request.helper.js
+++ client/src/helper/request.helper.js
@@ -6,9 +6,10 @@ export const request = axios.create({
   baseURL: AppConfig.apiUrl,
 });
 
-export const requestCommon = axios.create({
-  baseURL: `https://63ddb6c1f1af41051b085a9b.mockapi.io/member`,
+export const apiDanhSachSinhVien = axios.create({
+  baseURL: `https://63ddb6cff1af41051b085b6d.mockapi.io/sinh-vien`,
 });
-export const requestCommonStakeHolder = axios.create({
-  baseURL: `https://63ddb6c1f1af41051b085a9b.mockapi.io/user`,
+
+export const apiDanhSachGiangVien = axios.create({
+  baseURL: `https://63ddb6cff1af41051b085b6d.mockapi.io/giang-vien`,
 });
