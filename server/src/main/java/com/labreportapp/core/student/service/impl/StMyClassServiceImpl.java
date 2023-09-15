6a847ccf689da3d9469a3a941a08bc7ca315a5b7
codingaslife <hatran.devs@gmail.com>
2023-09-15 12:29:47 +0700

    complete build api register class

--- server/src/main/java/com/labreportapp/core/student/service/impl/StMyClassServiceImpl.java
+++ server/src/main/java/com/labreportapp/core/student/service/impl/StMyClassServiceImpl.java
@@ -1,17 +1,25 @@
 package com.labreportapp.core.student.service.impl;
 
 import com.labreportapp.core.common.base.SimpleEntityProjection;
+import com.labreportapp.core.student.model.request.StClassRequest;
 import com.labreportapp.core.student.model.request.StFindClassRequest;
+import com.labreportapp.core.student.model.response.StClassResponse;
 import com.labreportapp.core.student.model.response.StMyClassResponse;
+import com.labreportapp.core.student.repository.StClassRepository;
 import com.labreportapp.core.student.repository.StMyClassRepository;
+import com.labreportapp.core.student.repository.StStudentClassesRepository;
 import com.labreportapp.core.student.service.StMyClassService;
+import com.labreportapp.entity.Class;
 import com.labreportapp.entity.Semester;
+import com.labreportapp.entity.StudentClasses;
+import com.labreportapp.infrastructure.exception.rest.RestApiException;
 import com.labreportapp.repository.SemesterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * @author thangncph26123
@@ -21,6 +29,12 @@ public class StMyClassServiceImpl implements StMyClassService {
 
     @Autowired
     private StMyClassRepository stMyClassRepository;
+
+    @Autowired
+    private StClassRepository stClassRepository;
+
+    @Autowired
+    private StStudentClassesRepository stStudentClassesRepository;
 
     @Autowired
     @Qualifier(SemesterRepository.NAME)
@@ -33,5 +47,24 @@ public class StMyClassServiceImpl implements StMyClassService {
             req.setSemesterId(listSemester.get(0).getId());
         }
         return stMyClassRepository.getAllClass(req);
+    }
+
+    @Override
+    public void leaveClass(final StClassRequest req) {
+        Optional<Class> findCurrentMyClass = stClassRepository.findById(req.getIdClass());
+        Optional<StudentClasses> findStudentInClass = stStudentClassesRepository.
+                findStudentClassesByClassIdAndStudentId(req.getIdClass(), req.getIdStudent());
+        Optional<StClassResponse> checkTheFirstMeetingDate = stMyClassRepository.checkTheFirstMeetingDateByClass(req);
+
+        if (findStudentInClass.isPresent()){
+            if (checkTheFirstMeetingDate.isPresent()){
+                stStudentClassesRepository.delete(findStudentInClass.get());
+                findCurrentMyClass.get().setClassSize(findCurrentMyClass.get().getClassSize() - 1);
+                stClassRepository.save(findCurrentMyClass.get());
+            } else{
+                throw new RestApiException("Bạn không thể rời lớp học này");
+            }
+        }
+
     }
 }
