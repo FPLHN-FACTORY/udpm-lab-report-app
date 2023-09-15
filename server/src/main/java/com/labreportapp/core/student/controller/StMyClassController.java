678c995ac2d636e23f37f10e9894886794615111
codingaslife <hatran.devs@gmail.com>
2023-09-15 12:03:18 +0700

    update api StMyClass

--- server/src/main/java/com/labreportapp/core/student/controller/StMyClassController.java
+++ server/src/main/java/com/labreportapp/core/student/controller/StMyClassController.java
@@ -1,10 +1,14 @@
 package com.labreportapp.core.student.controller;
 
 import com.labreportapp.core.common.base.ResponseObject;
+import com.labreportapp.core.student.model.request.StClassRequest;
 import com.labreportapp.core.student.model.request.StFindClassRequest;
 import com.labreportapp.core.student.service.StMyClassService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +28,11 @@ public class StMyClassController {
     public ResponseObject getAllClass(final StFindClassRequest request) {
         System.out.println(request);
         return new ResponseObject(stMyClassService.getAllClass(request));
+    }
+
+    @DeleteMapping("/leave")
+    public ResponseObject leaveClass(final StClassRequest req){
+        stMyClassService.leaveClass(req);
+        return new ResponseObject(null);
     }
 }
