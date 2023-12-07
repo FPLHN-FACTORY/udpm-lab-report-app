40b9700eeff008192cbef4c7cb72d439e2412d60
nguyencongthang2509 <congthang25092003@gmail.com>
2023-12-07 15:36:59 +0700

    update code

--- client/src/labreportapp/pages/admin/level-management/LevelManagement.js
+++ client/src/labreportapp/pages/admin/level-management/LevelManagement.js
@@ -227,6 +227,7 @@ const LevelManagement = () => {
             Tên level:{" "}
             <Input
               type="text"
+              placeholder="Nhập tên level"
               value={name}
               onChange={(e) => {
                 setName(e.target.value);
