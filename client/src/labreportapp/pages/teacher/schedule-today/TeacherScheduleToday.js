b5c8710216d140c5d5f27b7ae07fa03914cdc6b1
hieundph25894 <hieundph25894@fpt.edu.vn>
2023-12-11 13:51:46 +0700

    - update code

--- client/src/labreportapp/pages/teacher/schedule-today/TeacherScheduleToday.js
+++ client/src/labreportapp/pages/teacher/schedule-today/TeacherScheduleToday.js
@@ -291,14 +291,14 @@ const TeacherScheduleToday = () => {
                 handleAddressChange(record.idMeeting, e.target.value);
               }}
             />
-            <span className="box-icon-square" onClick={updateAddress}>
-              <Tooltip title="Upload">
+            <Tooltip title="Cập nhật">
+              <span className="box-icon-square" onClick={updateAddress}>
                 <FontAwesomeIcon
                   icon={faUpload}
                   style={{ color: "white", fontSize: 21 }}
                 />
-              </Tooltip>
-            </span>
+              </span>
+            </Tooltip>
           </div>
         );
       },
