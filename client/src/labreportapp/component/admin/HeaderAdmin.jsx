1ddecb3d51db5121464e00299f72831fd9329ac8
Nguyễn Công Thắng <congthang25092003@gmail.com>
2023-12-12 17:22:14 +0700

    update code

--- client/src/labreportapp/component/admin/HeaderAdmin.jsx
+++ client/src/labreportapp/component/admin/HeaderAdmin.jsx
@@ -23,7 +23,7 @@ const HeaderAdminComponent = ({ collapsed, toggleCollapsed }) => {
           alignItems: "center",
           padding: "0 24px",
           color: "white",
-          background: "rgb(233, 192, 89)",
+          background: "rgb(233, 192, 89) !important",
         }}
       >
         <div style={{ display: "flex", alignItems: "center" }}>
