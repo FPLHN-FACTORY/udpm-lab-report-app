10fa0e5dbb7bd3810b7424eaa3a9396cf2bc0679
Nguyễn Công Thắng <congthang25092003@gmail.com>
2023-12-13 10:25:10 +0700

    update code

--- server/src/main/java/com/labreportapp/labreport/core/admin/service/impl/AdClassManagerServiceImpl.java
+++ server/src/main/java/com/labreportapp/labreport/core/admin/service/impl/AdClassManagerServiceImpl.java
@@ -638,6 +638,7 @@ public class AdClassManagerServiceImpl implements AdClassService {
                     loggerResponseList.add(loggerResponse);
                 }
             }
+            System.out.println(loggerResponseList.size() + " wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww");
             if (response.getStatus()) {
                 List<Class> listClass = repository.saveAll(listClassUpdate.values());
                 String nameSemester = loggerUtil.getNameSemesterByIdClass(listClass.get(0).getId());
